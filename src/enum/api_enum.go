59bde19ff062ae69386f4dbb2c5788561e49a3f9
ssssseeeee1007@gmail.com <1007suxie>
2021-03-30 15:15:59 +0800

    各種參數獲取完成

--- src/enum/api_enum.go
+++ src/enum/api_enum.go
@@ -1,9 +1,14 @@
 package enum
 
+import (
+	"reflect"
+)
+
 type api struct {
 	Operation  string `json:"operation"`
 	Function   string `json:"function"`
 	AlertLevel int    `json:"alertLevel"`
+	ModelName  string
 }
 
 var apiMap = make(map[string]api)
@@ -22,9 +27,35 @@ func addApiEnum(path string, operation string, function string, alertLevel int) 
 	api.Operation = operation
 	api.Function = function
 	api.AlertLevel = alertLevel
+	//api.ModelName = reflect.TypeOf(model).Name()
 	apiMap[path] = *api
+
+	//registerType(reflect.TypeOf(model))
+
 }
 
 func GetApiMap() map[string]api {
 	return apiMap
+}
+
+////////////////// 反射註冊 ////////////////////////
+
+//this is the registry of types by name
+var registry = map[string]reflect.Type{}
+
+// add a type to the registry
+func registerType(t reflect.Type) {
+	name := t.Name()
+	registry[name] = t
+}
+
+// create a new object by name, returning it as interface{}
+func NewByName(name string) interface{} {
+
+	t, found := registry[name]
+	if !found {
+		panic("name not found!")
+	}
+
+	return reflect.New(t).Elem().Interface()
 }
--- src/middleware/AdminLogger.go
+++ src/middleware/AdminLogger.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
@@ -11,8 +12,9 @@ import (
 
 func AdminLogger() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		test(c)
+		//test(c)
 		//printLog(c)
+		getParams(c)
 
 		adminLogInit(c)
 		c.Next()
@@ -60,27 +62,49 @@ func printLog(c *gin.Context) {
 	//fmt.Printf("data: %v\n", string(data))
 	c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(data)) // 关键点
 
-	accessLogMap := make(map[string]interface{})
-	//accessLogMap["request_time"]      = startTime
-	accessLogMap["request_method"] = c.Request.Method
-	accessLogMap["request_uri"] = c.Request.RequestURI
-	accessLogMap["request_proto"] = c.Request.Proto
-	accessLogMap["request_ua"] = c.Request.UserAgent()
-	accessLogMap["request_referer"] = c.Request.Referer()
-	accessLogMap["request_post_data"] = c.Request.PostForm.Encode()
-	accessLogMap["request_client_ip"] = c.ClientIP()
-	accessLogMap["request_body"] = c.Request.Body
+	//accessLogMap := make(map[string]interface{})
+	////accessLogMap["request_time"]      = startTime
+	//accessLogMap["request_method"] = c.Request.Method
+	//accessLogMap["request_uri"] = c.Request.RequestURI
+	//accessLogMap["request_proto"] = c.Request.Proto
+	//accessLogMap["request_ua"] = c.Request.UserAgent()
+	//accessLogMap["request_referer"] = c.Request.Referer()
+	//accessLogMap["request_post_data"] = c.Request.PostForm.Encode()
+	//accessLogMap["request_client_ip"] = c.ClientIP()
+	//accessLogMap["request_body"] = c.Request.Body
 
 	fmt.Println("========param in path========>")
-	fmt.Println(c.Params)
-	fmt.Println("========param not in path========>")
-	fmt.Println(c.Request.URL.Query())
-	fmt.Println("========param in body========>")
-	fmt.Println(bytes.NewBuffer(data))
-	fmt.Println("========param in x-www-form-urlencode========>")
-	fmt.Println(c.Request.Form)
+	fmt.Println(c.Params) // Param陣列，Param包含k和v
+	fmt.Println("========GET param not in path========>")
+	fmt.Println(c.Request.URL.Query()) //map
+	fmt.Println("========param in JSON========>")
+	var f interface{}
+	_ = json.Unmarshal(data, &f)
+	m := f.(map[string]interface{})
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+	fmt.Println("======== param in x-www-form-urlencode / form-data ========>")
+	if c.Request.Form == nil {
+		_ = c.Request.ParseMultipartForm(32 << 20)
+	}
+	for _, v := range c.Request.Form {
+		fmt.Println(v)
+	}
 
 }
 
-func test(c *gin.Context) {
+//func test(c *gin.Context) {
+//}
+
+func getParams(c *gin.Context) {
+	apiMap := enum.GetApiMap()
+	api, ok := apiMap[c.FullPath()]
+	if ok {
+		api = api
+	}
+}
+
+func why() interface{} {
+	return model.User{}
 }
